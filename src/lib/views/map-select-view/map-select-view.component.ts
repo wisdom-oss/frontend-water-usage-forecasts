89c7dc3d6ad37c3682e3187e59748fdbe755ec41
Tim Hesse <tim.hesse@uni-oldenburg.de>
2022-03-25 15:40:06 +0100

    map select now demands layer data from the map

--- src/lib/views/map-select-view/map-select-view.component.ts
+++ src/lib/views/map-select-view/map-select-view.component.ts
@@ -1,11 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from "@angular/core";
 
 @Component({
   selector: 'lib-map-select-view',
   templateUrl: './map-select-view.component.html'
 })
-export class MapSelectViewComponent implements OnInit {
+export class MapSelectViewComponent {
 
-  ngOnInit(): void {}
+  resolution?: string;
+
+  shapes?: string[];
+
+  selectionReady = false;
+
+  mapSelection(
+    selection: {layerName: string, resolution: string, shapes: string[]}
+  ) {
+    this.resolution = selection.resolution;
+    this.shapes = selection.shapes;
+    this.selectionReady = !!selection.shapes.length;
+  }
 
 }
