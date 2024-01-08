ae082d20b7279dd93f66d88dc57d660d737cc818
Piepmatz <3590829+cptpiepmatz@users.noreply.github.com>
2024-01-08 16:36:27 +0100

    map icons use local icons now

--- src/lib/map-icons.ts
+++ src/lib/map-icons.ts
@@ -1,10 +1,10 @@
 import {icon} from "leaflet";
 
 export const waterRightIcon = icon({
-  iconUrl: "https://unpkg.com/ionicons@5.5.2/dist/svg/water.svg",
+  iconUrl: "/ionicons/svg/water.svg",
   iconSize: [35, 35]
 });
 export const consumerIcon = icon({
-  iconUrl: "https://unpkg.com/ionicons@5.5.2/dist/svg/business.svg",
+  iconUrl: "/ionicons/svg/business.svg",
   iconSize: [35, 35]
 });
