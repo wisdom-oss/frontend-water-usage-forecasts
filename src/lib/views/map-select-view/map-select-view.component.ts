48fb1d119b515f7f621077165f448ca1c2aa768e
Tim 'Piepmatz' Hesse <git@cptpiepmatz.de>
2024-08-21 16:24:09 +0200

    replaced map with map2

--- src/lib/views/map-select-view/map-select-view.component.ts
+++ src/lib/views/map-select-view/map-select-view.component.ts
@@ -1,12 +1,19 @@
-import {Component, OnInit} from "@angular/core";
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from "@angular/core";
 import {ActivatedRoute} from "@angular/router";
-import {Resolution, BreadcrumbsService} from "common";
+import {Resolution, BreadcrumbsService, LayoutService, ResizeDirective, LayerConfig} from "common";
+import { Subscription, combineLatest } from "rxjs";
 
 @Component({
   selector: 'lib-map-select-view',
   templateUrl: './map-select-view.component.html'
 })
-export class MapSelectViewComponent implements OnInit {
+export class MapSelectViewComponent implements OnInit, AfterViewInit, OnDestroy {
+
+  LAYERS: LayerConfig.Input = [[
+    "view_nds_districts",
+    "view_nds_municipals"
+  ]];
+  height = "500px";
 
   // imported resolution from the map component
   Resolution = Resolution;
@@ -15,9 +22,14 @@ export class MapSelectViewComponent implements OnInit {
 
   selectionReady = false;
 
+  subscriptions: Subscription[] = [];
+
+  @ViewChild(ResizeDirective) buttonRow?: ResizeDirective;
+
   constructor(
     private breadcrumbs: BreadcrumbsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private layoutService: LayoutService,
   ) {}
 
   ngOnInit(): void {
@@ -25,7 +37,15 @@ export class MapSelectViewComponent implements OnInit {
       icon: "calendar",
       text: "water-usage-forecasts.breadcrumbs.water-consumption-prognosis",
       link: "/water-usage-forecasts"
-    })
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.fitMap();
+  }
+
+  ngOnDestroy(): void {
+    for (let sub of this.subscriptions) sub.unsubscribe();
   }
 
   mapSelection(
@@ -33,6 +53,20 @@ export class MapSelectViewComponent implements OnInit {
   ) {
     this.keys = selection.keys;
     this.selectionReady = !!selection.keys.length;
+  }
+
+  private fitMap() {
+    this.subscriptions.push(combineLatest([
+      this.layoutService.layout,
+      this.buttonRow!.resize
+    ]).subscribe(([{main}, buttonRow]) => {
+      // this value is definitely not real
+      if (buttonRow.height == 0) return;
+
+      let pads = 4 * 0.75 * this.layoutService.rem;
+      let restHeight = main!.height - (buttonRow.height + pads);
+      this.height = restHeight + "px";
+    }));
   }
 
 }
